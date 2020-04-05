34167366c8fe8dcdcd76ffec366b7b07cb72caed
salmanhiro <salman.consulting@outlook.com>
2020-04-05 22:00:39 +0700

    Added Konmari and minimalist method

--- konmari/konmari.py
+++ konmari/konmari.py
@@ -1,19 +1,23 @@
 import shutil as sh
 import os
 
-root_path = "./input"
-dest_path = "./notebook"
+root_path = "./input"#Modify if needed
+dest_path = "./json"#Modify if needed
  
 try:
     os.mkdir(dest_path)
-    print("Directory " , dest_path ,  " Created ") 
+    print("Directory " , dest_path ,  " Created ")
+    for dirpath, dnames, fnames in os.walk(root_path):    
+        for f in fnames:
+            if f.endswith(".json"): #Modify if needed
+                source_file_path =  os.path.join(dirpath, f)
+                dest_file_path   =  os.path.join(dest_path, f)
+                sh.copyfile(source_file_path, dest_file_path) 
 except FileExistsError:
     print("Directory " , dest_path ,  " Already Exists")
-
-
-for dirpath, dnames, fnames in os.walk(root_path):    
-    for f in fnames:
-        if f.endswith(".ipynb"):
-            source_file_path =  os.path.join(dirpath, f)
-            dest_file_path   =  os.path.join(dest_path, f)
-            sh.copyfile(source_file_path, dest_file_path)
+    for dirpath, dnames, fnames in os.walk(root_path):    
+        for f in fnames:
+            if f.endswith(".json"): #Modify if needed
+                source_file_path =  os.path.join(dirpath, f)
+                dest_file_path   =  os.path.join(dest_path, f)
+                sh.copyfile(source_file_path, dest_file_path)
--- minimalist/minimalist.py
+++ minimalist/minimalist.py
@@ -3,7 +3,7 @@ import os
 print("Every day you should RUN THIS SCRIPT every new program came!")
 days = int(input("How many days since you commit? "))
 root_path = "./input" #modify this line if needed
-dest_path = "./notebook" #modify this line if needed
+dest_path = "./json" #modify this line if needed
  
 try:        
     os.mkdir(dest_path)
@@ -11,7 +11,7 @@ try:
     for dirpath, dnames, fnames in os.walk(root_path):    
         for f in fnames:
             for i in range(days):
-                if f.endswith(".ipynb"): #modify this line if needed
+                if f.endswith(".json"): #modify this line if needed
                     source_file_path =  os.path.join(dirpath, f)
                     dest_file_path   =  os.path.join(dest_path, f)
                     sh.copyfile(source_file_path, dest_file_path)
@@ -20,7 +20,7 @@ except FileExistsError:
     for dirpath, dnames, fnames in os.walk(root_path):    
         for f in fnames:
             for i in range(days):
-                if f.endswith(".ipynb"): #modify this line if needed
+                if f.endswith(".json"): #modify this line if needed
                     source_file_path =  os.path.join(dirpath, f)
                     dest_file_path   =  os.path.join(dest_path, f)
                     sh.copyfile(source_file_path, dest_file_path)
